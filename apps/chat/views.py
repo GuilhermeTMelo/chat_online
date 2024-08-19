6f14ba12ec9dbf68cc1c08ec0b5504e79a76965e
Guilherme Torres Melo <guigi1005@gmail.com>
2024-08-19 13:45:08 -0300

    preparação do frontend

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,14 +1,18 @@
+import json
 from django.shortcuts import render
 from .models import Message, Room
+from django.views.generic.detail import DetailView
 
-# Create your views here.
-def index(request):
-
-    messages = Message.objects.all()
-    rooms = Room.objects.all()
-
-    return render(request, 'index.html', {
-        'messages': messages,
+def home(request):
+    rooms = Room.objects.all().order_by('-created_at')
+    return render(request, 'chat/home.html', {
         'rooms': rooms,
-        }
-    )
+    })
+
+class RoomDetailView(DetailView):
+    model = Room
+    template_name = 'chat/list-messages.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
