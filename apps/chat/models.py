4d6b5a25504fe925611f8678f6ec276259ad78b5
Guilherme Torres Melo <guigi1005@gmail.com>
2024-08-19 14:44:53 -0300

    ajuste de rooms

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -14,13 +14,14 @@ class Room(models.Model):
     def __str__(self):
         return self.title
 
+
 class Message(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    text=models.TextField()
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} {self.text}"
+        return f"{self.user.first_name}: {self.text}"
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -16,3 +16,20 @@ class RoomDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+def send_message(request, pk):
+    data = json.loads(request.body)
+    room = Room.objects.get(id=pk)
+    new_message = Message.objects.create(user = request.user, text=data['message'])
+    room.messages.add(new_message)
+    return render(request, 'chat/message.html', {
+        'm': new_message
+    })
+
+
+def create_room(request):
+    data = json.loads(request.body)
+    room = Room.objects.create(user=request.user, title=data['title'])
+    return render(request, 'chat/room.html', {
+        'r': room
+    })
